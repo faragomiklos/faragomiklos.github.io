13ae158654e5593901175091f33e6d618010fd35
faragomiklos <farago_miklos@t-online.hu>
2020-08-10 16:20:35 +0200

    validalas 1

--- elarningHonlap/js/megrendeles.js
+++ elarningHonlap/js/megrendeles.js
@@ -9,14 +9,53 @@ function kivalasztottFeltet() {
             return feltet.value;
         }
     }
+}
 
+function kivalasztottSzosz() {
+    let szoszValaszt = document.getElementById("szosz");
+    return szoszValaszt[szoszValaszt.selectedIndex].value;
 }
 
 function osszegSzamol() {
     let alapar = 1000;
+    let fizetendoMegjelenit = document.querySelector("span.fizetendo");
     let mennyisegBemenet = document.querySelector("input[name='mennyiseg']");
     let feltet = kivalasztottFeltet();
-    let osszeg = parseInt(mennyisegBemenet.value) * alapar + parseInt(feltet);
-    alert("Fizetendő: " + osszeg + "Ft");
+    let szosz = kivalasztottSzosz();
+    let osszeg = parseInt(mennyisegBemenet.value) * (alapar + parseInt(feltet) + parseInt(szosz));
+
+    /* alert("Fizetendő: " + osszeg + "Ft"); */
+
+    fizetendoMegjelenit.innerHTML = osszeg;
+}
+
+function szovegEllenoriz(ellenorizendo, hossz) {
+    let vissza;
+    let szoveg = document.getElementById(ellenorizendo);
+    if (szoveg.value.trim().length > hossz) szoveg.value = szoveg.value.trim();
+    else alert("A megadott adat nem megfelelő !");
+}
+
+
+function emailEllenoriz() {
+    let mailforma = /^([A-z0-9\_\.\-]+)@([a-z0-9\_\-\.]+).([a-z]{2,})$/;
+    
+    let email = document.getElementById("email");
+    if (email.value.match(mailforma)) {
+        console.log("Nem megfelelő email cím");
+    }
+    else {
+        alert("You have entered an invalid email address!");
+    }
+}
+
+function telefonszamEllenoriz() {
+    let telefonszam = Number(document.getElementById("telszam").value);
+    if(isNaN(telefonszam)){
+        alert("Nem megfelelő telefonszám!");
+    }
+    else{
+        if(telefonszam.length < 10) alert("Nem megfelelő telefonszám!");
+    }
 }
 
